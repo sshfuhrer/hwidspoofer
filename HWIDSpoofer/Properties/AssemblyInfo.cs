93e5f443cdcf88f58d4199690200d1f6656c6ee0
haunted <74558190+sshfuhrer@users.noreply.github.com>
2024-05-16 14:31:28 +0300

    🎗

---
+++ HWIDSpoofer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения о сборке определяются следующим набором атрибутов.
+// Измените значения этих атрибутов, чтобы изменить сведения, связанные с этой сборкой.
+[assembly: AssemblyTitle("HWIDSpoofer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HWIDSpoofer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если тип в этой сборке необходимо 
+// сделать доступным из COM, установите атрибут ComVisible в значение true для этого типа.
+[assembly: ComVisible(false)]
+
+// Если этот проект предоставляет COM-объекты, следующий GUID будет идентификатором типа библиотеки
+[assembly: Guid("b95c1bfb-4393-42f5-a718-97a8ef6c1ed8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Номер редакции
+//
+// Можно указать все значения или задать для номеров редакции и номера сборки значение по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
